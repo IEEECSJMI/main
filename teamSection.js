5e896d1cc931d854818e9645ad67c70381e3165c
TahirReyaz <tahir200213@st.jmi.ac.in>
2021-07-16 18:04:30 +0530

    Added typewriter.js

--- teamSection.js
+++ teamSection.js
@@ -1,3 +1,24 @@
-$("button").click(function(){
-    $(".teamMembers").slideToggle();
+$("#mlTeamBtn").click(function(){
+    $("#mlTeam").slideToggle();
 });
+
+$("#dsaTeamBtn").click(function(){
+    $("#dsaTeam").slideToggle();
+});
+
+// var app = $("#typewriterTarget");
+var app = document.getElementById('typewriterTarget');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();

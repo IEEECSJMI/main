43aff28ae70e532e041e02cc1c71dc98996a0378
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2021-07-16 15:21:10 +0530

    navbar init

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-'use strict'
+// navbar thingy
+$(document).ready(function () {
+  let isSidebarActive=false;
+
+  function toggleSidebar() {
+    isSidebarActive = $(".nav-right .button").hasClass("active");
+    console.log(isSidebarActive);
+    if (isSidebarActive) {
+      $(".button").removeClass("active");
+      $(".sidebar").removeClass("animate__slideInRight hidden-xs");
+      $(".sidebar").addClass("animate__slideOutRight");
+      $(".sidebar-item").removeClass("active");
+    } else {
+      $(".button").addClass("active");
+      $(".sidebar").removeClass("animate__slideOutRight hidden-xs");
+      $(".sidebar").addClass("animate__slideInRight");
+      $(".sidebar-item").addClass("active");
+      console.log("activated side abr")
+    }
+  }
+
+  $(".button").on("click tap", function () {
+    toggleSidebar();
+  });
+
+});
+

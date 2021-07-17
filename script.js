42517f40530f7ab8093e37862f91e57980568f72
MD Rashid Hussain <mdrashid.hussain.786.01@gmail.com>
2021-07-17 08:25:36 +0530

    typing effect

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-'use strict'
+const code = document.querySelectorAll("code")
+for(let i=0; i<code.length; i++){
+    new TypeIt(code[i], {
+        speed: 50,
+        loop: false,
+        startDelay: 50
+    }).go();    
+}
+
+
+

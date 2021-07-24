0361d4efdedb518bae0706dd88326e15e0e370c8
Shoeb Ilyas <shoebilyas123@gmail.com>
2021-07-24 11:03:14 +0530

    •Created a new background texture image and added it to the websites background.

--- teamSection.js
+++ teamSection.js
@@ -3,15 +3,15 @@ $(".teamMembers").hide();
 
 // Toggle Slide up and down when the team members button is clicked
 $("#mlTeamBtn").click(function () {
-    $("#mlTeam").slideToggle();
+  $("#mlTeam").slideToggle();
 });
 
 $("#dsaTeamBtn").click(function () {
-    $("#dsaTeam").slideToggle();
+  $("#dsaTeam").slideToggle();
 });
 
 $("#webdTeamBtn").click(function () {
-    $("#webdTeam").slideToggle();
+  $("#webdTeam").slideToggle();
 });
 
 // Tabs
@@ -19,22 +19,22 @@ $("#webdTeamBtn").click(function () {
 $("#mlTeamTabButton").click();
 
 function openTeam(evt, teamName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(teamName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(teamName).style.display = "block";
+  evt.currentTarget.className += " active";
 }

97e614f29997141fa88a1914b4763953eaec221c
TahirReyaz <tahir200213@st.jmi.ac.in>
2021-07-24 15:10:03 +0530

    Changed structure of other two teams

--- teamSection.js
+++ teamSection.js
@@ -1,16 +1,3 @@
-// When the page loads, Hide all the div which show the images of the team members
-$(".teamMembers").hide();
-$("#mlTeam").slideToggle();
-
-// Toggle Slide up and down when the team members button is clicked
-$("#dsaTeamBtn").click(function () {
-  $("#dsaTeam").slideToggle();
-});
-
-$("#webdTeamBtn").click(function () {
-  $("#webdTeam").slideToggle();
-});
-
 // Tabs
 
 $("#mlTeamTabButton").click();
